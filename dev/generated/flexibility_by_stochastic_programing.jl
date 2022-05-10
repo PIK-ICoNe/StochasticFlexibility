108d02cb18bb9bd5c0be4a87aa4fcc2df5670f9a
Documenter.jl <documenter@juliadocs.github.io>
2022-05-10 13:17:47 +0000

    build based on 281bd74

--- dev/generated/flexibility_by_stochastic_programing.jl
+++ dev/generated/flexibility_by_stochastic_programing.jl
@@ -1,6 +1,25 @@
-cd(@__DIR__)
+# Everything runs in the Project environment on the basepath
+
+basepath = realpath(joinpath((@__DIR__, "..")))
+
 using Pkg
-Pkg.activate(".")
+Pkg.activate(basepath)
+Pkg.instantiate()
+
+using DataFrames
+using CSV
+using Clp
+
+include(joinpath((basepath, "src", "sp_model.jl")))
+include(joinpath((basepath, "src", "plot_utils.jl")))
+include(joinpath((basepath, "src", "evaluation_utils.jl")))
+
+offset = 6531
+timesteps = 1:168
+
+pv = CSV.read("timeseries/basic_example.csv", DataFrame)[timesteps .+ offset, 3]
+wind = CSV.read("timeseries/basic_example.csv", DataFrame)[timesteps .+ offset, 4]
+demand = CSV.read("timeseries/basic_example.csv", DataFrame)[timesteps .+ offset, 2]
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
