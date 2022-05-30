257f2219115ecf388a2ab35521f186d5a2f56bbb
Documenter.jl <documenter@juliadocs.github.io>
2022-05-30 14:34:52 +0000

    build based on 5b21ad5

--- dev/generated/flexibility_by_stochastic_programing.jl
+++ dev/generated/flexibility_by_stochastic_programing.jl
@@ -8,14 +8,17 @@ Pkg.activate(basepath)
 using DataFrames
 using CSV
 using Clp
-using Statistics
+using Statistics;
+
+using Random
+Random.seed!(1)
 
 include(joinpath(basepath, "src", "sp_model.jl"))
 include(joinpath(basepath, "src", "plot_utils.jl"))
-include(joinpath(basepath, "src", "evaluation_utils.jl"))
+include(joinpath(basepath, "src", "evaluation_utils.jl"));
 
-offset = 6531
-timesteps = 1:168
+offset = 24*7*14
+timesteps = 1:(24*7*2)
 
 data = CSV.read(joinpath(basepath, "timeseries", "basic_example.csv"), DataFrame)
 
@@ -23,9 +26,12 @@ pv = data[timesteps .+ offset, 3]
 wind = data[timesteps .+ offset, 4]
 demand = data[timesteps .+ offset, 2]
 
-data = nothing # Free the memory
+data = nothing; # Free the memory
 
-#
+plt = plot(timesteps, pv .* (mean(demand) / mean(pv)), label="PV")
+plot!(plt, timesteps, wind.* (mean(demand) / mean(wind)), label="Wind")
+plot!(plt, timesteps, demand, label="Demand")
+plt
 
 pars = copy(default_es_pars)
 
@@ -38,15 +44,12 @@ pars[:c_wind] = 800.
 pars[:c_sto_op] = 0.00001
 
 heatdemand = copy(demand)./300.
-heatdemand0 = zeros(length(demand))
-
-#
+heatdemand0 = zeros(length(demand));
 
 es = define_energy_system(pv, wind, demand, heatdemand; p = pars, strict_flex = true)
 
-#
-
 sp_no_flex = instantiate(es, no_flex_pseudo_sampler(), optimizer = Clp.Optimizer)
+set_silent(sp_no_flex)
 
 optimize!(sp_no_flex)
 
@@ -54,38 +57,29 @@ no_flex_decision = optimal_decision(sp_no_flex)
 
 objective_value(sp_no_flex)
 
-#
+analysis_window = 150+1:150+48
 
-cost_pos, pot_pos, cost_neg, pot_neg = analyze_flexibility_potential(sp_no_flex, 20:50)
+cost_pos, pot_pos, cost_neg, pot_neg = analyze_flexibility_potential(sp_no_flex, analysis_window)
 
-plot_flexibility(20:50, cost_pos, pot_pos, cost_neg, pot_neg)
-
-#
+plot_flexibility(analysis_window, cost_pos, pot_pos, cost_neg, pot_neg)
 
 n = 100
 F_max = average_hourly_demand * 0.1 # Have unaticipated demand equal to 10% of our typical demand
 t_max = length(pv) - es.parameters[2].defaults[:recovery_time]
 
-scens = simple_flex_sampler(n, F_max, t_max)
-
-#
+scens = simple_flex_sampler(n, F_max, t_max);
 
 sp_flex = instantiate(es, scens, optimizer = Clp.Optimizer)
+set_silent(sp_flex)
 
 evaluate_decision(sp_flex, no_flex_decision)
-
-#
 
 es_reg = define_energy_system(pv, wind, demand, heatdemand; p = pars, strict_flex = false)
 
 sp_reg_flex = instantiate(es_reg, scens, optimizer = Clp.Optimizer)
+set_silent(sp_reg_flex)
 
-
-#
-
-evaluate_decision(sp_reg_flex, no_flex_decision)
-
-#
+evaluate_decision(sp_reg_flex, no_flex_decision, scens[1])
 
 investments_nf = get_investments(sp_no_flex)
 fix_investment!(sp_flex, investments_nf)
@@ -95,35 +89,24 @@ optimize!(sp_flex)
 optimize!(sp_reg_flex)
 
 flex_no_invest_decision = optimal_decision(sp_flex)
-reg_flex_no_invest_decision = optimal_decision(sp_reg_flex)
-
-#
+reg_flex_no_invest_decision = optimal_decision(sp_reg_flex);
 
 evaluate_decision(sp_reg_flex, flex_no_invest_decision)
 
-#
 evaluate_decision(sp_reg_flex, reg_flex_no_invest_decision)
-
-#
 
 relative_flex_cost = evaluate_decision(sp_reg_flex, flex_no_invest_decision) / objective_value(sp_no_flex) - 1.
 
-#
+cost_pos_flex, pot_pos_flex, cost_neg_flex, pot_neg_flex = analyze_flexibility_potential(sp_flex, analysis_window)
 
-cost_pos_flex, pot_pos_flex, cost_neg_flex, pot_neg_flex = analyze_flexibility_potential(sp_flex, 20:50)
-
-plot_flexibility(20:50, cost_pos_flex, pot_pos_flex, cost_neg_flex, pot_neg_flex)
-
-#
+plot_flexibility(analysis_window, cost_pos_flex, pot_pos_flex, cost_neg_flex, pot_neg_flex)
 
 plt_av = plot();
 flexibility_availability!(plt_av, pot_pos, label = "positive flexbility unaware");
 flexibility_availability!(plt_av, pot_pos_flex, label = "positive flexibility aware");
 flexibility_availability!(plt_av, pot_neg, label = "negative flexibility unaware");
 flexibility_availability!(plt_av, pot_neg_flex, label = "negative flexibility aware");
-display(plt_av)
-
-#
+plt_av
 
 unfix_investment!(sp_flex, investments_nf)
 unfix_investment!(sp_reg_flex, investments_nf)
@@ -132,18 +115,13 @@ optimize!(sp_flex)
 optimize!(sp_reg_flex)
 
 flex_invest_decision = optimal_decision(sp_flex)
-reg_flex_invest_decision = optimal_decision(sp_reg_flex)
+reg_flex_invest_decision = optimal_decision(sp_reg_flex);
 
 evaluate_decision(sp_reg_flex, flex_invest_decision)
 
-#
 evaluate_decision(sp_reg_flex, reg_flex_invest_decision)
 
-#
-
 relative_flex_cost_inv = evaluate_decision(sp_reg_flex, flex_invest_decision) / objective_value(sp_no_flex) - 1.
-
-#
 
 relative_flex_cost_inv / relative_flex_cost - 1.
 
