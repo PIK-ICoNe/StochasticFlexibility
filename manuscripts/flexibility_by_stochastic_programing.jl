03111b4dc4691c529bec3c72d727cdede68f7f88
Frank Hellmann <hellmann@pik-potsdam.de>
2022-06-02 13:58:49 +0200

    Minor tweaks

--- manuscripts/flexibility_by_stochastic_programing.jl
+++ manuscripts/flexibility_by_stochastic_programing.jl
@@ -12,7 +12,7 @@ using Clp
 using Statistics;
 
 using Random
-Random.seed!(1)
+Random.seed!(1);
 
 #-
 
@@ -58,9 +58,10 @@ heatdemand = heatdemand_data[timesteps .+ offset, 1]
 data = nothing; # Free the memory
 heatdemand_data = nothing;
 
-plt = plot(timesteps, pv .* (mean(demand) / mean(pv)), label="PV")
-plot!(plt, timesteps, wind.* (mean(demand) / mean(wind)), label="Wind")
-plot!(plt, timesteps, demand, label="Demand")
+plt = plot(timesteps, pv .* (mean(demand) / mean(pv)), label="PV (unitless)")
+plot!(plt, timesteps, wind.* (mean(demand) / mean(wind)), label="Wind (unitless)")
+plot!(plt, timesteps, heatdemand, label="Heat Demand")
+plot!(plt, timesteps, demand, label="Electric Demand")
 plt
 #-
 
@@ -74,13 +75,10 @@ pars = copy(default_es_pars)
 average_hourly_demand = mean(demand)
 
 pars[:recovery_time] = 24
-pars[:c_storage] = 100.
+pars[:c_storage] = 70.
 pars[:c_pv] = 300.
-pars[:c_wind] = 800.
-pars[:c_sto_op] = 0.00001
-
-#heatdemand = copy(demand)./300.
-#heatdemand0 = zeros(length(demand));
+pars[:c_wind] = 600.
+pars[:c_sto_op] = 0.00001;
 
 #-
 
