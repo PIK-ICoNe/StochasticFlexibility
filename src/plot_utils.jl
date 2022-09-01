fdd895ea983944b50f68ced2dc26b3841f23abb1
Frank <hellmann@pik-potsdam.de>
2022-09-01 11:15:49 +0200

    Starting refactor of model to have cleaner event windows

--- src/plot_utils.jl
+++ src/plot_utils.jl
@@ -26,12 +26,12 @@ function plot_results(sp, pv, w, el_d; plot_span = 1:length(pv), s=1, el_balance
 
     stor_charge = value.(sp[1, :sto_soc])
     plot!(plt_sto, plot_span, stor_charge[plot_span], label="global storage charge")
-    plot!(plt_sto, (t_xi):(t_xi+recovery_time-1), value.(sp[2, :sto_soc2],s), label=string("stochastic storage charge")*string(s), linestyle=:dash, linewidth=2)
+    plot!(plt_sto, (t_xi):(t_xi+recovery_time), value.(sp[2, :sto_soc2],s), label=string("stochastic storage charge")*string(s), linestyle=:dash, linewidth=2)
 
     for var in el_balance_vars
         plot!(plt, plot_span, value.(sp[1, var])[plot_span], label=string(var))
         var2 = Symbol(string(var)*"2")
-        plot!(plt, (t_xi+1):(t_xi+recovery_time), value.(sp[2, var2], s), label=string(var)*" 2nd stage, s = "*string(s), linestyle=:dash, linewidth=2)
+        plot!(plt, (t_xi+1):(t_xi+recovery_time + 1), value.(sp[2, var2], s), label=string(var)*" 2nd stage, s = "*string(s), linestyle=:dash, linewidth=2)
 
     end
     return plot(plt_invest, plt, plt_sto, layout = (3,1))
@@ -45,8 +45,8 @@ function plot_heat_layer(sp, heatdemand; plot_span = 1:length(heatdemand), s = 1
     plot!(plt_heat, plot_span, COP*value.(sp[1, :flow_energy2heat])[plot_span], label = "heatpump")
     plot!(plt_heat, plot_span, value.(sp[1, :heat_sto_soc])[plot_span], label = "heat storage SOC")
     plot!(plt_heat, plot_span, value.(sp[1, :heat_sto_to_bus])[plot_span]-value.(sp[1, :heat_sto_from_bus])[plot_span], label = "heat storage use")
-    # plot!(plt_heat, (t_xi+1):(t_xi+recovery_time), COP*value.(sp[2, :flow_energy2heat2], s), label = "heatpump$s", linestyle=:dash, linewidth=2)
-    # plot!(plt_heat, (t_xi+1):(t_xi+recovery_time), value.(sp[2, :heat_sto_in2], s)-value.(sp[2, :heat_sto_out2], s), label = "heat storage use $s", linestyle=:dash, linewidth=2)
+    # plot!(plt_heat, (t_xi+1):(t_xi+1+recovery_time), COP*value.(sp[2, :flow_energy2heat2], s), label = "heatpump$s", linestyle=:dash, linewidth=2)
+    # plot!(plt_heat, (t_xi+1):(t_xi+1+recovery_time), value.(sp[2, :heat_sto_in2], s)-value.(sp[2, :heat_sto_out2], s), label = "heat storage use $s", linestyle=:dash, linewidth=2)
 
 end
 
@@ -55,13 +55,13 @@ function plot_recovery_window_deviation(sp; s = 1)
     plt_sto = plot()
     t_xi = scenarios(sp)[s].data.t_xi
     recovery_time = sp.stages[2].parameters[:recovery_time]
-    plot!(plt_gc, value.(sp[1, :flow_energy2heat])[t_xi+1:t_xi+recovery_time], label = "global flow_energy2heat")
+    plot!(plt_gc, value.(sp[1, :flow_energy2heat])[t_xi+1:t_xi+1+recovery_time], label = "global flow_energy2heat")
     plot!(plt_gc, value.(sp[2, :flow_energy2heat2], s), label = "stochastic flow_energy2heat")
-    plot!(plt_gc, -value.(sp[1, :gci])[t_xi+1:t_xi+recovery_time]+value.(sp[2, :gci2], s), label = "global grid connection use")
-    plot!(plt_gc, value.(sp[2, :gco2], s) - value.(sp[1, :gco])[t_xi+1:t_xi+recovery_time], xlabel = "time after the event, h", label = "stochastic grid connection use")
+    plot!(plt_gc, -value.(sp[1, :gci])[t_xi+1:t_xi+1+recovery_time]+value.(sp[2, :gci2], s), label = "global grid connection use")
+    plot!(plt_gc, value.(sp[2, :gco2], s) - value.(sp[1, :gco])[t_xi+1:t_xi+1+recovery_time], xlabel = "time after the event, h", label = "stochastic grid connection use")
     #plot!(plt_gc, -value.(sp[1, :gco])[t_xi+1:t_xi+recovery_time]+value.(sp[2, :gco2], s), xlabel = "time after the event, h", label = "gco2-gco")
     
-    stor_charge = value.(sp[1, :sto_soc])[t_xi+1:t_xi+recovery_time]
+    stor_charge = value.(sp[1, :sto_soc])[t_xi+1:t_xi+1+recovery_time]
     stor_charge2 = value.(sp[2, :sto_soc2], s)
     
     plot!(plt_sto, stor_charge2-stor_charge, label = "soc2-soc")
@@ -108,8 +108,8 @@ function plot_scenario_debug(sp, s; window_start=-2, window_end=2, vars = ["gci"
     t_xi = scenarios(sp)[s].data.t_xi
     recovery_time = sp.stages[2].parameters[:recovery_time]
 
-    recovery_window = t_xi:t_xi+recovery_time-1
-    plot_window = t_xi+window_start:t_xi+recovery_time-1+window_end
+    recovery_window = t_xi:t_xi+recovery_time
+    plot_window = t_xi+window_start:t_xi+recovery_time+window_end
 
     plots = []
 
@@ -136,7 +136,7 @@ function plot_outcome_debug(sp_base, t_xi, s_xi, F_xi; window_start=-2, window_e
         return termination_status(sp)
     end
 
-    recovery_window = t_xi:t_xi+length(sp[:gco2])-1
+    recovery_window = t_xi:t_xi+length(sp[:gco2])
     plot_window = t_xi+window_start:t_xi+length(sp[:gco2])-1+window_end
 
     plots = []
