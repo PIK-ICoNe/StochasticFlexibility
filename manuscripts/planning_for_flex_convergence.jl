f829e7f0cf81d535b7fd90f188acdd622c632d6e
Frank <hellmann@pik-potsdam.de>
2022-09-10 14:39:29 +0200

    resampling perfromance fixed...

--- manuscripts/planning_for_flex_convergence.jl
+++ manuscripts/planning_for_flex_convergence.jl
@@ -90,7 +90,8 @@ plot_heat_layer(sp_bkg, heatdemand)
 #-
 
 #=
-Now we evaluate the system for different frequencies of flexibility
+Now we set up the system for different number of samples twice, the first for optimizing,
+the second as a resampled version for validating.
 =#
 
 #-
@@ -100,126 +101,58 @@ delta_t = 7*24 - recovery_time
 pars[:scens_in_year] = t_max / (delta_t + recovery_time + 1);
 
 
-# n_samples = 10:10:100
-# sps = [sp_bkg for n in n_samples]
+n_samples = [collect(2:2:10); collect(15:5:25)]
 
-
-# Threads.@threads for i in eachindex(n_samples)
-#     n = round(Int, n_samples[i] * pars[:scens_in_year])
-#     scens = poisson_events_with_offset(n, delta_t, recovery_time, F_max, t_max)
-#     es = define_energy_system(pv, wind, demand, heatdemand; p = pars, override_no_scens_in_year = true)
-#     sp = instantiate(es, scens, optimizer = Clp.Optimizer)
-#     set_silent(sp)
-#     sps[i] = sp
-# end
-#-
-
-#=
-Optimize only the operational schedule for flexibility
-=#
-
-# stime = time()
-
-# Threads.@threads for i in eachindex(sps)
-#     optimize!(sps[i])
-# end
-
-# costs = objective_value.(sps)
-
-# println("Optimization performed in $(time() - stime) seconds")
-# #-
-
-# cost_plot = plot()
-# cost_plot = plot!(n_samples, costs)
-#-
-
-#=
-
-n_samples = 10:10:100
-
-julia> costs
-10-element Vector{Float64}:
- 2.681880876640262e8
- 2.6834200438138604e8
- 2.684729576379481e8
- 2.68640961683499e8
- 2.6865071432198125e8
- 2.6885427663025177e8
- 2.6889814868351394e8
- 2.689452143285036e8
- 2.689299155883828e8
- 2.690139815363563e8
-
-=#
-
-
-n_samples2 = [collect(2:2:10); collect(15:5:25)]
-sps2 = [sp_bkg for n in n_samples2]
+sps = [sp_bkg for n in n_samples]
+sps_resampled = [sp_bkg for n in n_samples]
 
 t_max = length(pv) - 24
 F_max = 10000.
 delta_t = 7*24 - recovery_time
 pars[:scens_in_year] = t_max / (delta_t + recovery_time + 1);
 
-for i in eachindex(n_samples2)
-    n = round(Int, n_samples2[i] * pars[:scens_in_year])
+for i in eachindex(n_samples)
+    n = round(Int, n_samples[i] * pars[:scens_in_year])
     scens = poisson_events_with_offset(n, delta_t, recovery_time, F_max, t_max)
+    scens_resampled = poisson_events_with_offset(n, delta_t, recovery_time, F_max, t_max)
     es = define_energy_system(pv, wind, demand, heatdemand; p = pars, override_no_scens_in_year = true)
     sp = instantiate(es, scens, optimizer = Clp.Optimizer)
+    sp_resampled = instantiate(es, scens_resampled, optimizer = Clp.Optimizer)
     set_silent(sp)
-    sps2[i] = sp
+    set_silent(sp_resampled)
+    sps[i] = sp
+    sps_resampled[i] = sp_resampled
 end
 
 #-
+
+# Optimize the first set of models...
 
 stime = time()
 
-Threads.@threads for i in eachindex(sps2)
-    optimize!(sps2[i])
+Threads.@threads for i in eachindex(sps)
+    optimize!(sps[i])
 end
 
-costs2 = objective_value.(sps2)
+costs = objective_value.(sps)
 
 println("Optimization performed in $(time() - stime) seconds")
 #-
-# Evaluate on resampled scenarios - This seems to be taking enormously long right now...
+# Evaluate on the second set.
 
-eval_dec_resample = zeros(length(n_samples2))
+costs_resampled = zeros(length(n_samples))
 
-for i in eachindex(n_samples2)
-    n = round(Int, n_samples2[i] * pars[:scens_in_year])
-    scens = poisson_events_with_offset(n, delta_t, recovery_time, F_max, t_max)
-    eval_dec_resample[i] = mean([evaluate_decision(sps2[i], optimal_decision(sps2[i]), scen) for scen in scens])
-    println(eval_dec_resample[i])
+Threads.@threads  for i in eachindex(n_samples)
+    costs_resampled[i] = evaluate_decision(sps_resampled[i], optimal_decision(sps[i]))
 end
 
 #-
-#=
 
-On the original sample
-
-mean([evaluate_decision(sp, optimal_decision(sp), scen) for scen in scens])
-
-evaluates to the objective value
-
-scens = poisson_events_with_offset(2, delta_t, recovery_time, F_max, t_max)
-es = define_energy_system(pv, wind, demand, heatdemand; p = pars, override_no_scens_in_year = true)
-sp = instantiate(es, scens, optimizer = Clp.Optimizer)
-set_silent(sp)
-optimize!(sp)
-ov = objective_value(sp)
-eds = [evaluate_decision(sp, optimal_decision(sp), scen) for scen in scens]
-isapprox(mean(eds), ov) # true
-
-=#
-
+cost_plot = plot()
+cost_plot = plot!(n_samples, costs ./ cost_bkg)
 #-
 
 cost_plot = plot()
-cost_plot = plot!(n_samples2, costs2 ./ cost_bkg)
-#-
-
-cost_plot = plot()
-cost_plot = plot!(n_samples2, eval_dec_resample ./ cost_bkg)
+cost_plot = plot!(n_samples, costs_resampled ./ cost_bkg)
 #-
 
