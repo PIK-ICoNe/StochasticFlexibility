0cb6dbb03da28dd9ed9964115de4799f7e96e8b2
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2022-10-17 18:07:22 +0200

    Update investment sensitivity experiment file Clean up

--- experiments/investment_sensitivity.jl
+++ experiments/investment_sensitivity.jl
@@ -1,20 +1,25 @@
 ## Everything runs in the Project environment on the basepath
 
+#=
+In this experiment we analyze investment decision's sensitivity to sampling.
+We variate request samples in two ways.
+1. scen_freq defines the frequency of events in the sample. 
+For example, scen_freq = 24 means that we expect an event roughly every day.
+With scen_freq we calculate events_per_scen.
+2. n_samples lets us modify the total sample size. We generate n_samples*events_per_scen requests.
+
+For each pair of (n_samples,scen_freq) in given limits we instantiate a stochastic programming model.
+The model is optimized. We save investment decisions, objective value and the sample.
+We repeat this n_runs times to get a distribution.
+=#
 basepath = realpath(joinpath(@__DIR__, ".."))
 
 using Pkg
 Pkg.activate(basepath)
 # Pkg.instantiate()
 
-using DataFrames
-using CSV
-using Tables
-using Clp
-using Statistics;
-using StochasticPrograms
-
-using Random
-
+#= Include the file containing all the dependecies and optimization functions.
+=#
 include(joinpath(basepath, "experiments", "stochastic_optimization_setup.jl"));
 
 #-
@@ -27,7 +32,7 @@ pv, wind, demand, heatdemand = load_basic_example(timesteps);
 
 #=
 Next we continue the set up. Our model comes with default parameters,
-which we slightly adjust here. We use some arbitrary values to define a dummy heat demand.
+which we slightly adjust here.
 =#
 
 pars = copy(default_es_pars)
@@ -44,8 +49,17 @@ pars[:penalty] = 1000000.
 We set up runs with variating scenario frequency and number of scenarios
 =#
 
-for n_samples in 10:40:200
-    for scen_freq in 24:30*24
-        optimize_sp(pv, wind, demand, heatdemand, pars, n_samples, scen_freq, savefiles = true)
+run_id = time()
+mkdir("results/$run_id")
+savepath = joinpath(basepath, "results/$run_id")
+
+for n_samples in [[1];collect(2:2:10); collect(15:5:25)]
+    for scen_freq in 24:24:30*24
+        param_id = "$n_samples_$scen_freq"
+        savefiles = Dict(([var => joinpath(savepath, string(var)*param_id*".csv")
+        for var in [:runtime, :scen, :costs, :inv]]))
+            Threads.@threads for n in 1:n_runs
+            optimize_sp(pv, wind, demand, heatdemand, pars, n_samples, scen_freq, savefiles = savefiles)
+        end
     end
 end
--- experiments/stochastic_optimization_setup.jl
+++ experiments/stochastic_optimization_setup.jl
@@ -11,7 +11,7 @@ include(joinpath(basepath, "src", "sp_model.jl"))
 include(joinpath(basepath, "src", "evaluation_utils.jl"))
 include(joinpath(basepath, "src", "data_load.jl"));
 
-function optimize_sp(pv, wind, demand, heatdemand, pars, n_samples, scen_freq; F_min = 3000., F_max = 10000., t_max_offset = 24, savefiles = false)
+function optimize_sp(pv, wind, demand, heatdemand, pars, n_samples, scen_freq; F_min = 3000., F_max = 10000., t_max_offset = 24, savefiles = nothing)
     t_max = minimum((length(pv), length(wind), length(demand), length(heatdemand))) - t_max_offset
     delta_t = scen_freq - recovery_time
     pars[:event_per_scen] = t_max / (delta_t + recovery_time + 1)
@@ -25,11 +25,22 @@ function optimize_sp(pv, wind, demand, heatdemand, pars, n_samples, scen_freq; F
     println("Model setup and instantiation performed in $(time() - stime) seconds")
     stime = time()
     optimize!(sp)
-    println("Model optimized in $(time() - stime) secons")
+    runtime = time() - stime
+    println("Model optimized in $runtime seconds")
     investments = get_investments(sp)
-    if savefiles
-        CSV.write(joinpath(basepath, "results", "investments$n_$stime.csv"), DataFrame(investments), header = string.(keys(investments)), append = true)
-        CSV.write(joinpath(basepath, "results", "costs$n_$stime.csv"), Tables.table([objective_value(sp)]), header = ["Objective value"], append = true)
-        CSV.write(joinpath(basepath, "results", "scens$n_$stime.csv"), DataFrame([s.data for s in scens]), header = string.(keys(scens[1].data)), append = true)
+    if !isnothing(savefiles)
+        if :scen in keys(savefiles)
+            CSV.write(savefiles[:scen], DataFrame([s.data for s in scens]), header = string.(keys(scens[1].data)), append = true)
+        end
+        if :inv in keys(savefiles)
+            CSV.write(savefiles[:inv], DataFrame(investments), header = string.(keys(investments)), append = true)
+        end
+        if :costs in keys(savefiles)
+            CSV.write(savefiles[:costs], Tables.table([objective_value(sp)]), header = ["Objective value"], append = true)
+        end
+        if :runtime in keys(savefiles)
+            CSV.write(savefiles[:runtime], Tables.table([runtime]), header = ["Runtime"], append = true)
+        end
     end
+    return sp
 end
