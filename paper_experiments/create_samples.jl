33b394bb080e6b8604a2b41993b93de0ba3d2d4e
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2023-05-05 15:49:02 +0200

    Add adaptive n_samples to flex_cost sampler

--- paper_experiments/create_samples.jl
+++ paper_experiments/create_samples.jl
@@ -5,9 +5,11 @@ basepath = realpath(joinpath(@__DIR__, ".."))
 
 using Pkg
 Pkg.activate(basepath)
+include(joinpath(basepath, "src", "sp_model.jl"))
+using JSON
 #-
 # Pkg.instantiate()
-params = [(5000., 48), (7500., 144), (25000., 240)]
+params = [(5000., 48, 25), (7500., 144,70), (25000., 240, 120)]
 
 savepath = joinpath(basepath, "samples")
 if !isdir(savepath)
@@ -16,11 +18,12 @@ end
 
 t_max_offset = 24
 t_max = 24*365 - t_max_offset
-n_samples = 20
+#n_samples = 30
 recovery_time = 12#pars[:recovery_time]
+#-
 for i in eachindex(params)
-    F, scen_freq = params[i]
-    savepath_i = joinpath(savepath, "scen_freq$scen_freq")
+    F, scen_freq, n_samples = params[i]
+    savepath_i = joinpath(savepath, "scen_freq$(scen_freq)_n_$(n_samples)")
     if !isdir(savepath_i)
         mkdir(savepath_i)
     end
@@ -29,7 +32,7 @@ for i in eachindex(params)
         delta_t = scen_freq - recovery_time
         event_per_scen = t_max / (delta_t + recovery_time + 1)
         n = round(Int, n_samples * event_per_scen)
-        scens = poisson_events_with_offset(n, delta_t, recovery_time, F, t_max, F_min = 3000.)
+        scens = poisson_events_with_offset(n, delta_t, recovery_time, F, t_max, F_min = F*0.6)
         scens_dict = [Dict((:probability => s.probability, :t_xi => s.data[:t_xi], 
         :F_xi => s.data[:F_xi], :s_xi => s.data[:s_xi])) for s in scens]
         open(joinpath(savepath_i, "sample$j.json"), "w") do f
