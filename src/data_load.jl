69bcd0973bd62e3e5ae2920f1182ebd934611946
Ekaterina Zolotarevskaia <zkatya123@gmail.com>
2023-08-31 10:23:22 +0200

    update loading timeseries

--- src/data_load.jl
+++ src/data_load.jl
@@ -1,12 +1,55 @@
 using DataFrames
 using CSV
 
-function load_max_boegl(timesteps; offset = 0, heat = nothing)
+# pass heat = "district" for district heating, heat = "when2heat" for germany
+    # "district" is default
+function load_max_boegl(timesteps; offset = 0, heat = nothing) 
     pv_data = CSV.read(joinpath(basepath, "timeseries/validation_usecase", "pv_Halle18.csv"), DataFrame, header = false)
     wind_data =  CSV.read(joinpath(basepath, "timeseries/validation_usecase", "wind_Karholz.csv"), DataFrame, header = false)
     demand_data =  CSV.read(joinpath(basepath, "timeseries/validation_usecase", "demand_Industriepark.csv"), DataFrame, header = false)
     if !isnothing(heat)
-        heatdemand_data = CSV.read(joinpath(basepath, "timeseries", "heatdemand.csv"), DataFrame)
+        if heat == "when2heat"
+            heatdemand_data = CSV.read(joinpath(basepath, "timeseries", "heatdemand.csv"), DataFrame)
+            heatdemand = heatdemand_data[timesteps .+ offset, 1]./40.
+        else
+            heatdemand_data = CSV.read(joinpath(basepath, "timeseries/validation_usecase", "csv_heatdemand_apartment_block(KFW40)_300km2_12300MWhperyear.csv"), DataFrame, header = false)
+            heatdemand = heatdemand_data[timesteps .+ offset, 1]
+        end
+    else
+        heatdemand = zeros(length(timesteps))
+    end
+    pv = pv_data[timesteps .+ offset, 1]
+    wind = wind_data[timesteps .+ offset, 1]
+    demand = demand_data[timesteps .+ offset, 1]
+
+    pv_data = nothing;
+    wind_data = nothing;
+    demand_data = nothing; # Free the memory
+
+    pars = copy(default_es_pars)
+
+    pars[:recovery_time] = 12
+    pars[:c_storage] = 300.
+    pars[:c_pv] = 800.
+    pars[:c_wind] = 1150.
+    pars[:c_in] = 0.165
+    pars[:c_out] = 0.02
+    pars[:asset_lifetime] = 5.
+    pars[:inv_budget] = 10000000.
+    pars[:sto_ef_ch] = 0.97
+    pars[:sto_ef_dis] = 0.97
+    pars[:feedincap] = 1e7
+    pars[:max_pv] = 10^4
+    pars[:max_wind] = 10^4;
+    return pv, wind, demand, heatdemand, pars
+end
+
+function load_max_boegl_district_heating(timesteps; offset = 0, heat = false, heat_timeseries = "district")
+    pv_data = CSV.read(joinpath(basepath, "timeseries/validation_usecase", "pv_Halle18.csv"), DataFrame, header = false)
+    wind_data =  CSV.read(joinpath(basepath, "timeseries/validation_usecase", "wind_Karholz.csv"), DataFrame, header = false)
+    demand_data =  CSV.read(joinpath(basepath, "timeseries/validation_usecase", "demand_Industriepark.csv"), DataFrame, header = false)
+    if heat
+        heatdemand_data = CSV.read(joinpath(basepath, "timeseries/validation_usecase", "csv_heatdemand_apartment_block(KFW40)_300km2_12300MWhperyear.csv"), DataFrame, header = false)
         heatdemand = heatdemand_data[timesteps .+ offset, 1]
     else
         heatdemand = zeros(length(timesteps))
@@ -31,7 +74,9 @@ function load_max_boegl(timesteps; offset = 0, heat = nothing)
     pars[:inv_budget] = 10000000.
     pars[:sto_ef_ch] = 0.97
     pars[:sto_ef_dis] = 0.97
-    pars[:feedincap] = 1e7;
+    pars[:feedincap] = 1e7
+    pars[:max_pv] = 10^4
+    pars[:max_wind] = 10^4;
     return pv, wind, demand, heatdemand, pars
 end
 
